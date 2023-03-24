a299f9af750441b773820f6b31d86d2f3ad2cffc
ChrisKnowles <christianjknowles@gmail.com>
2023-03-24 11:53:22 +0000

    event listnere added to inputField

--- main.js
+++ main.js
@@ -9,5 +9,34 @@ async function getDefinition() {
 
 getDefinition();
 
+let inputField = document.getElementById("userInputWord");
+let inputValue = ``;
+
+inputField.addEventListener('keyup', (event) =>  {
+    inputValue = event.target.value;
+    console.log(inputValue);
+});
+
+
+
+
 //Define where the location of the definition of hello is stored within the json packet.
 //It starts within data. 0, meanings, 2, definitions, 0, definition.
+
+// The PLAN
+
+// create a method for user to input their own word
+    // create an input field on the HTML file - the text inputted is tracked within this field ✅
+    // create a submit button✅
+    // add an event listener to the input field to track the user input
+    // add an event listener to the submit button to trigger the fetch function
+    // use the word inputted by user as the input to the getDefinition function
+// alter the api so that url is not specific to hello
+
+// what happens if the api does not contain the word the user has input
+        // create a conditional statement to check if the word is in the api
+        // return word not recognized try another word
+// display the definition on the page
+    // create a div on the HTML file
+    // create a p tag on the HTML file
+    // make the text within the p update with the definition
