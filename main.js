1743cfef7c7c2872f32f258dc1684877a8228e04
Anastasia Adamoudi <53370412+AnastasiaAdamoudi@users.noreply.github.com>
2023-03-24 13:53:59 +0000

    added event listener to the submit word button and updated the API

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
 //Create an async function.
 let data = ``;
 async function getDefinition() {
-    const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en_GB/hello`);
+    const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en_GB/${inputValue}`);
     const data = await response.json();
     console.log(data);
-    console.log(data[0].meanings[2].definitions[0].definition);
+    console.log(data[0].meanings[0].definitions[0].definition);
+    //console.log(data.querySelector("#definitions"));
+  
 };
-
-getDefinition();
 
 let inputField = document.getElementById("userInputWord");
 let inputValue = ``;
 
-inputField.addEventListener('keyup', (event) =>  {
+inputField.addEventListener('keyup', (event) => {
     inputValue = event.target.value;
     console.log(inputValue);
 });
 
+let submitButton = document.getElementById("submitWord");
+submitButton.addEventListener('click', getDefinition);
 
-
+//getDefinition();
 
 //Define where the location of the definition of hello is stored within the json packet.
 //It starts within data. 0, meanings, 2, definitions, 0, definition.
@@ -28,9 +30,9 @@ inputField.addEventListener('keyup', (event) =>  {
 // create a method for user to input their own word
     // create an input field on the HTML file - the text inputted is tracked within this field ✅
     // create a submit button✅
-    // add an event listener to the input field to track the user input
-    // add an event listener to the submit button to trigger the fetch function
-    // use the word inputted by user as the input to the getDefinition function
+    // add an event listener to the input field to track the user input✅
+    // add an event listener to the submit button to trigger the fetch function✅
+    // use the word inputted by user as the input to the getDefinition function✅
 // alter the api so that url is not specific to hello
 
 // what happens if the api does not contain the word the user has input
